03d7624ff2fe1c0bac8f76991acc46650df21dfc
sivasankarreddyb1997 <106857309+sivasankarreddyb1997@users.noreply.github.com>
2022-09-14 21:35:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let url = window.location.origin;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Hello Team</h4>
+        <h5>This application is running in Cloud Run </h5>
+        URL: {url}
       </header>
     </div>
   );
